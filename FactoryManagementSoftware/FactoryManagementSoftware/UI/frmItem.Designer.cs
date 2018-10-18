93a944882fe4ca56a0bbe434e74e028f08fa8e9d
JunOng <junong09@gmail.com>
2018-10-18 14:11:46 +0800

    Item Page data grid view edit

--- FactoryManagementSoftware/FactoryManagementSoftware/UI/frmItem.Designer.cs
+++ FactoryManagementSoftware/FactoryManagementSoftware/UI/frmItem.Designer.cs
@@ -41,6 +41,10 @@
             this.lblSearch = new System.Windows.Forms.Label();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
             this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.dgvcItemCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvcItemName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvcQty = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvcOrd = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dgvItem)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
@@ -49,8 +53,8 @@
             // lblItemCode
             // 
             this.lblItemCode.AutoSize = true;
-            this.lblItemCode.Location = new System.Drawing.Point(88, 69);
-            this.lblItemCode.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblItemCode.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblItemCode.Location = new System.Drawing.Point(61, 51);
             this.lblItemCode.Name = "lblItemCode";
             this.lblItemCode.Size = new System.Drawing.Size(136, 32);
             this.lblItemCode.TabIndex = 0;
@@ -58,18 +62,18 @@
             // 
             // txtItemCode
             // 
-            this.txtItemCode.Location = new System.Drawing.Point(234, 69);
-            this.txtItemCode.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.txtItemCode.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtItemCode.Location = new System.Drawing.Point(203, 51);
             this.txtItemCode.Name = "txtItemCode";
-            this.txtItemCode.Size = new System.Drawing.Size(300, 38);
+            this.txtItemCode.Size = new System.Drawing.Size(367, 38);
             this.txtItemCode.TabIndex = 1;
             this.txtItemCode.TextChanged += new System.EventHandler(this.txtItemCode_TextChanged);
             // 
             // lblItemName
             // 
             this.lblItemName.AutoSize = true;
-            this.lblItemName.Location = new System.Drawing.Point(80, 159);
-            this.lblItemName.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblItemName.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblItemName.Location = new System.Drawing.Point(53, 118);
             this.lblItemName.Name = "lblItemName";
             this.lblItemName.Size = new System.Drawing.Size(144, 32);
             this.lblItemName.TabIndex = 2;
@@ -77,18 +81,20 @@
             // 
             // txtItemName
             // 
-            this.txtItemName.Location = new System.Drawing.Point(234, 159);
-            this.txtItemName.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.txtItemName.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtItemName.Location = new System.Drawing.Point(203, 118);
             this.txtItemName.Name = "txtItemName";
-            this.txtItemName.Size = new System.Drawing.Size(300, 38);
+            this.txtItemName.Size = new System.Drawing.Size(367, 38);
             this.txtItemName.TabIndex = 3;
             this.txtItemName.TextChanged += new System.EventHandler(this.txtItemName_TextChanged);
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(94, 567);
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(86, 584);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(120, 65);
+            this.btnDelete.Size = new System.Drawing.Size(111, 48);
             this.btnDelete.TabIndex = 4;
             this.btnDelete.Text = "DELETE";
             this.btnDelete.UseVisualStyleBackColor = true;
@@ -96,9 +102,11 @@
             // 
             // btnInsert
             // 
-            this.btnInsert.Location = new System.Drawing.Point(267, 567);
+            this.btnInsert.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.Location = new System.Drawing.Point(326, 584);
+            this.btnInsert.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnInsert.Name = "btnInsert";
-            this.btnInsert.Size = new System.Drawing.Size(120, 65);
+            this.btnInsert.Size = new System.Drawing.Size(111, 48);
             this.btnInsert.TabIndex = 5;
             this.btnInsert.Text = "ADD";
             this.btnInsert.UseVisualStyleBackColor = true;
@@ -106,9 +114,11 @@
             // 
             // btnReset
             // 
-            this.btnReset.Location = new System.Drawing.Point(414, 567);
+            this.btnReset.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(459, 584);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnReset.Name = "btnReset";
-            this.btnReset.Size = new System.Drawing.Size(120, 65);
+            this.btnReset.Size = new System.Drawing.Size(111, 48);
             this.btnReset.TabIndex = 6;
             this.btnReset.Text = "RESET";
             this.btnReset.UseVisualStyleBackColor = true;
@@ -119,29 +129,35 @@
             this.dgvItem.AllowUserToAddRows = false;
             this.dgvItem.AllowUserToDeleteRows = false;
             this.dgvItem.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvItem.Location = new System.Drawing.Point(606, 129);
+            this.dgvItem.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgvcItemCode,
+            this.dgvcItemName,
+            this.dgvcQty,
+            this.dgvcOrd});
+            this.dgvItem.Location = new System.Drawing.Point(644, 118);
+            this.dgvItem.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dgvItem.Name = "dgvItem";
             this.dgvItem.ReadOnly = true;
             this.dgvItem.RowTemplate.Height = 24;
             this.dgvItem.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvItem.Size = new System.Drawing.Size(888, 503);
+            this.dgvItem.Size = new System.Drawing.Size(872, 514);
             this.dgvItem.TabIndex = 7;
             this.dgvItem.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvItem_CellMouseDoubleClick);
             // 
             // txtItemSearch
             // 
-            this.txtItemSearch.Location = new System.Drawing.Point(703, 69);
-            this.txtItemSearch.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.txtItemSearch.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtItemSearch.Location = new System.Drawing.Point(730, 51);
             this.txtItemSearch.Name = "txtItemSearch";
-            this.txtItemSearch.Size = new System.Drawing.Size(791, 38);
+            this.txtItemSearch.Size = new System.Drawing.Size(549, 38);
             this.txtItemSearch.TabIndex = 9;
             this.txtItemSearch.TextChanged += new System.EventHandler(this.txtItemSearch_TextChanged);
             // 
             // lblSearch
             // 
             this.lblSearch.AutoSize = true;
-            this.lblSearch.Location = new System.Drawing.Point(607, 69);
-            this.lblSearch.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblSearch.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSearch.Location = new System.Drawing.Point(638, 51);
             this.lblSearch.Name = "lblSearch";
             this.lblSearch.Size = new System.Drawing.Size(86, 32);
             this.lblSearch.TabIndex = 8;
@@ -155,9 +171,39 @@
             // 
             this.errorProvider2.ContainerControl = this;
             // 
+            // dgvcItemCode
+            // 
+            this.dgvcItemCode.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dgvcItemCode.HeaderText = "Code";
+            this.dgvcItemCode.Name = "dgvcItemCode";
+            this.dgvcItemCode.ReadOnly = true;
+            // 
+            // dgvcItemName
+            // 
+            this.dgvcItemName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dgvcItemName.HeaderText = "Name";
+            this.dgvcItemName.Name = "dgvcItemName";
+            this.dgvcItemName.ReadOnly = true;
+            // 
+            // dgvcQty
+            // 
+            this.dgvcQty.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.dgvcQty.HeaderText = "Quantity";
+            this.dgvcQty.Name = "dgvcQty";
+            this.dgvcQty.ReadOnly = true;
+            this.dgvcQty.Width = 105;
+            // 
+            // dgvcOrd
+            // 
+            this.dgvcOrd.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.dgvcOrd.HeaderText = "Order";
+            this.dgvcOrd.Name = "dgvcOrd";
+            this.dgvcOrd.ReadOnly = true;
+            this.dgvcOrd.Width = 83;
+            // 
             // frmItem
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1582, 803);
             this.Controls.Add(this.txtItemSearch);
@@ -170,8 +216,8 @@
             this.Controls.Add(this.lblItemName);
             this.Controls.Add(this.txtItemCode);
             this.Controls.Add(this.lblItemCode);
-            this.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "frmItem";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Item";
@@ -199,6 +245,10 @@
         private System.Windows.Forms.Label lblSearch;
         private System.Windows.Forms.ErrorProvider errorProvider1;
         private System.Windows.Forms.ErrorProvider errorProvider2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvcItemCode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvcItemName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvcQty;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgvcOrd;
     }
 }
 
--- FactoryManagementSoftware/FactoryManagementSoftware/UI/frmItem.cs
+++ FactoryManagementSoftware/FactoryManagementSoftware/UI/frmItem.cs
@@ -2,27 +2,23 @@
 using FactoryManagementSoftware.DAL;
 using FactoryManagementSoftware.UI;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FactoryManagementSoftware
 {
     public partial class frmItem : Form
     {
+        
+        itemBLL uItem = new itemBLL();
+        itemDAL dalItem = new itemDAL();
+
+        #region Load or Reset Form
+
         public frmItem()
         {
             InitializeComponent();
         }
-
-        itemBLL uItem = new itemBLL();
-        itemDAL dalItem = new itemDAL();
 
         private void frmItem_FormClosed(object sender, FormClosedEventArgs e)
         {
@@ -37,8 +33,18 @@ namespace FactoryManagementSoftware
         private void loadData()
         {
             DataTable dt = dalItem.Select();
-            dgvItem.DataSource = dt;
+            //dgvItem.DataSource = dt;
+            dgvItem.Rows.Clear();
+            foreach (DataRow item in dt.Rows)
+            {
+                int n = dgvItem.Rows.Add();
+                dgvItem.Rows[n].Cells["dgvcItemCode"].Value = item["item_code"].ToString();
+                dgvItem.Rows[n].Cells["dgvcItemName"].Value = item["item_name"].ToString();
+                dgvItem.Rows[n].Cells["dgvcQty"].Value = item["item_qty"].ToString();
+                dgvItem.Rows[n].Cells["dgvcOrd"].Value = item["item_ord"].ToString();
+            }
         }
+
         private void resetForm()
         {
             txtItemCode.ReadOnly = false;
@@ -50,97 +56,36 @@ namespace FactoryManagementSoftware
             loadData();
         }
 
+        #endregion
+
+        #region Data Grid View
         private void dgvItem_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
         {
             //get the index of particular row
             int rowIndex = e.RowIndex;
-            txtItemCode.Text = dgvItem.Rows[rowIndex].Cells["item_code"].Value.ToString();
-            txtItemName.Text = dgvItem.Rows[rowIndex].Cells["item_name"].Value.ToString();
+            txtItemCode.Text = dgvItem.Rows[rowIndex].Cells["dgvcItemCode"].Value.ToString();
+            txtItemName.Text = dgvItem.Rows[rowIndex].Cells["dgvcItemName"].Value.ToString();
 
+            btnInsert.Text = "UPDATE";
             btnDelete.Show();
             txtItemCode.ReadOnly = true;
 
         }
 
-        private void btnInsert_Click(object sender, EventArgs e)
-        {
-            DialogResult dialogResult = MessageBox.Show("Are you sure want to insert data to database?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+        #endregion
 
-            if (dialogResult == DialogResult.Yes && Validation())
-            {
-                if (IfProductsExists(txtItemCode.Text))
-                {
-
-                    //Update data
-                    uItem.item_code = txtItemCode.Text;
-                    uItem.item_name = txtItemName.Text;
-                    uItem.item_updtd_date = DateTime.Now;
-                    uItem.item_updtd_by = 0;
-
-                    //Updating data into database
-                    bool success = dalItem.Update(uItem);
-
-                    //if data is updated successfully then the value = true else false
-                    if (success == true)
-                    {
-                        //data updated successfully
-                        MessageBox.Show("item successfully updated ");
-                        resetForm();
-                    }
-                    else
-                    {
-                        //failed to update user
-                        MessageBox.Show("Failed to updated item");
-                    }
-                }
-                else
-                {
-                    //Add data
-                    uItem.item_code = txtItemCode.Text;
-                    uItem.item_name = txtItemName.Text;
-                    uItem.item_added_date = DateTime.Now;
-                    uItem.item_added_by = -1;
-
-                    //Inserting Data into Database
-                    bool success = dalItem.Insert(uItem);
-                    //If the data is successfully inserted then the value of success will be true else false
-                    if (success == true)
-                    {
-                        //Data Successfully Inserted
-                        MessageBox.Show("Item successfully created");
-                        resetForm();
-                    }
-                    else
-                    {
-                        //Failed to insert data
-                        MessageBox.Show("Failed to add new item");
-                    }
-                }
-
-                 
-            }
-        }
-
-        private bool IfProductsExists(String productCode)
-        {
-            DataTable dt = dalItem.Search(productCode);
-
-            if (dt.Rows.Count > 0)
-                return true;
-            else
-                return false;
-        }
+        #region data validation
 
         private bool Validation()
         {
             if (string.IsNullOrEmpty(txtItemCode.Text))
             {
-                
+
                 errorProvider1.SetError(txtItemCode, "Item Code Required");
             }
             if (string.IsNullOrEmpty(txtItemName.Text))
             {
-                
+
                 errorProvider2.SetError(txtItemName, "Item Name Required");
             }
 
@@ -154,6 +99,16 @@ namespace FactoryManagementSoftware
 
         }
 
+        private bool IfProductsExists(String productCode)
+        {
+            DataTable dt = dalItem.Search(productCode);
+
+            if (dt.Rows.Count > 0)
+                return true;
+            else
+                return false;
+        }
+
         private void txtItemCode_TextChanged(object sender, EventArgs e)
         {
             errorProvider1.Clear();
@@ -164,29 +119,91 @@ namespace FactoryManagementSoftware
             errorProvider2.Clear();
         }
 
+        #endregion
+
+        #region Function: Insert/Delete/Reset/Search
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+
+            if (Validation())
+            {
+                DialogResult dialogResult = MessageBox.Show("Are you sure want to insert data to database?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    if (IfProductsExists(txtItemCode.Text))
+                    {
+
+                        //Update data
+                        uItem.item_code = txtItemCode.Text;
+                        uItem.item_name = txtItemName.Text;
+                        uItem.item_updtd_date = DateTime.Now;
+                        uItem.item_updtd_by = 0;
+
+                        //Updating data into database
+                        bool success = dalItem.Update(uItem);
+
+                        //if data is updated successfully then the value = true else false
+                        if (success == true)
+                        {
+                            //data updated successfully
+                            MessageBox.Show("item successfully updated ");
+                            resetForm();
+                        }
+                        else
+                        {
+                            //failed to update user
+                            MessageBox.Show("Failed to updated item");
+                        }
+                    }
+                    else
+                    {
+                        //Add data
+                        uItem.item_code = txtItemCode.Text;
+                        uItem.item_name = txtItemName.Text;
+                        uItem.item_added_date = DateTime.Now;
+                        uItem.item_added_by = -1;
+
+                        //Inserting Data into Database
+                        bool success = dalItem.Insert(uItem);
+                        //If the data is successfully inserted then the value of success will be true else false
+                        if (success == true)
+                        {
+                            //Data Successfully Inserted
+                            MessageBox.Show("Item successfully created");
+                            resetForm();
+                        }
+                        else
+                        {
+                            //Failed to insert data
+                            MessageBox.Show("Failed to add new item");
+                        }
+                    }
+                } 
+            }
+        }
+
         private void btnDelete_Click(object sender, EventArgs e)
         {
-            DialogResult dialogResult = MessageBox.Show("Are you sure want to delete?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-
-            if (dialogResult == DialogResult.Yes)
-            {
-                uItem.item_code = txtItemCode.Text;
-
-                bool success = dalItem.Delete(uItem);
-
-                if (success == true)
+                DialogResult dialogResult = MessageBox.Show("Are you sure want to delete?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dialogResult == DialogResult.Yes)
                 {
-                    //item deleted successfully
-                    MessageBox.Show("Item deleted successfully");
-                    resetForm();
-                }
-                else
-                {
-                    //Failed to delete item
-                    MessageBox.Show("Failed to delete item");
-                }
-            }
-                
+                    uItem.item_code = txtItemCode.Text;
+
+                    bool success = dalItem.Delete(uItem);
+
+                    if (success == true)
+                    {
+                        //item deleted successfully
+                        MessageBox.Show("Item deleted successfully");
+                        resetForm();
+                    }
+                    else
+                    {
+                        //Failed to delete item
+                        MessageBox.Show("Failed to delete item");
+                    }
+                }       
         }
 
         private void btnReset_Click(object sender, EventArgs e)
@@ -209,7 +226,16 @@ namespace FactoryManagementSoftware
             {
                 //show user based on keywords
                 DataTable dt = dalItem.Search(keywords);
-                dgvItem.DataSource = dt;
+                //dgvItem.DataSource = dt;
+                dgvItem.Rows.Clear();
+                foreach (DataRow item in dt.Rows)
+                {
+                    int n = dgvItem.Rows.Add();
+                    dgvItem.Rows[n].Cells["dgvcItemCode"].Value = item["item_code"].ToString();
+                    dgvItem.Rows[n].Cells["dgvcItemName"].Value = item["item_name"].ToString();
+                    dgvItem.Rows[n].Cells["dgvcQty"].Value = item["item_qty"].ToString();
+                    dgvItem.Rows[n].Cells["dgvcOrd"].Value = item["item_ord"].ToString();
+                }
 
             }
             else
@@ -218,5 +244,7 @@ namespace FactoryManagementSoftware
                 loadData();
             }
         }
+
+        #endregion
     }
 }
